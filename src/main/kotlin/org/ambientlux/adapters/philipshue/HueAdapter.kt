2857c3d42446ed33759cfe43f9dcd79b7e0b039b
Valentin Trifonov <valentin.trifonov@hotmail.de>
2019-03-17 00:11:34 +0100

    configure logging

--- src/main/kotlin/org/ambientlux/adapters/philipshue/HueAdapter.kt
+++ src/main/kotlin/org/ambientlux/adapters/philipshue/HueAdapter.kt
@@ -3,17 +3,20 @@ package org.ambientlux.adapters.philipshue
 import org.ambientlux.adapters.LightsAdapter
 import org.ambientlux.service.domain.LightStatus
 import org.ambientlux.service.domain.LightsGroup
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 
 @Component
 class HueAdapter constructor(properties: HueProperties, restClient: RestClient) : LightsAdapter {
 
+    private val log = LoggerFactory.getLogger(javaClass)
+
     init {
         // TODO figure out how to do logging
-        println("Hue apiKey: ${properties.apiKey}")
+        log.debug("Hue apiKey: ${properties.apiKey}")
 
         val groups = restClient.getGroups(properties.apiKey)
-        println("Groups: $groups")
+        log.info("Groups: $groups")
     }
 
     override fun getLightsGroup(groupId: String): LightsGroup {
